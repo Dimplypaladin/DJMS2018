91051faf88b2324fa098f43ccd790c88adc1e6ae
Dimplypaladin <40444579+Dimplypaladin@users.noreply.github.com>
2018-06-24 20:46:47 -0500

    Update User.java

--- User.java
+++ User.java
@@ -1,16 +1,1 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Entity;
 
-/**
- *
- * @author Danny
- */
-public class User {
-    public User(String user, String pass){
-        
-    }
-}
